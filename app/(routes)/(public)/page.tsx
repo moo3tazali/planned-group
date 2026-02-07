9529be63a33a40d0c5f522f5c4261acb6603bd0c
Moataz Ali <moo3tazali@gmail.com>
2026-02-07 13:29:47 +0200

    Update favicon and add logo component to the public page; remove unused SVG files to streamline assets.

--- app/(routes)/(public)/page.tsx
+++ app/(routes)/(public)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import { Logo } from '@/icons/logo';
 
 export default function Page() {
   return (
@@ -6,6 +7,7 @@ export default function Page() {
       <h1 className='text-4xl font-extrabold'>Planned Group</h1>
       <p className='text-lg'>Planned Group is a company that plans groups.</p>
       <Button>Click me</Button>
+      <Logo className='size-20' />
     </div>
   );
 }
